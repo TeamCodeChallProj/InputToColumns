79c2211de77efa7eb62b3b499d9a71d3e4be5ac7
alacielx <62450706+alacielx@users.noreply.github.com>
2022-07-12 11:22:18 -0500

    InputRead

--- src/main/java/InputRead.java
+++ src/main/java/InputRead.java
@@ -1,19 +1,21 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class InputRead {
-    public static Stream<String> readFile(){
+    public static Stream<String> readFile(String filePath){
         Stream<String> fileStream;
+        //filePath = (new File("").getAbsolutePath() + "/src/main/java/inputTest.txt");
 
         try {
-            fileStream = Files.lines(Paths.get(new File("").getAbsolutePath() + "/src/main/java/inputTest.txt"));
+            fileStream = Files.lines(Paths.get(filePath));
         } catch (IOException e) {
-            System.out.println("no file found");
+            System.out.println("File not found");
             return null;
         }
 
