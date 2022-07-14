106c80d66539f2c94c99b0e288436e682333a796
Lexi Henson <LexiHensonDev@gmail.com>
2022-07-14 14:53:06 -0500

    Update pom.xml & finish arg parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,44 @@
+import java.util.Optional;
+
 public class Main
 {
+	public static void main(String[] args)
+	{
+		boolean sorted;
+		int columns;
+		String fileName;
+		
+		Optional<Boolean> optionalSorted;
+		Optional<Integer> optionalColumns;
+		Optional<String> optionalFileName;
+		
+		for (String arg : args)
+		{
+			var argVal = arg.substring(arg.indexOf('=') + 1);
+			
+			System.out.println(argVal);
+			
+			if (arg.startsWith("--sorted="))
+			{
+				optionalSorted = Optional.of(argVal.equals("true"));
+			}
+			if (arg.startsWith("--columns="))
+			{
+				try
+				{
+					optionalColumns = Optional.of(Integer.parseInt(argVal));
+				}
+				catch (NumberFormatException exception)
+				{
+					System.out.println(argVal + "is not a valid number.");
+				}
+			}
+			if (arg.startsWith("--file="))
+			{
+				optionalFileName = Optional.of(argVal);
+			}
+		}
+		
+		
+	}
 }
