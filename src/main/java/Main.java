d31b08eca6fa5ce9669c4c9dd880c44d740e1dd9
Lexi Henson <LexiHensonDev@gmail.com>
2022-07-15 09:55:35 -0500

    Style changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 public class Main
 {
+	
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args)
 	{
 		boolean sorted;
@@ -15,8 +17,8 @@ public class Main
 		String fileName;
 		
 		Optional<Boolean> optionalSorted = Optional.empty();
-		Optional<Integer> optionalColumns= Optional.empty();
-		Optional<String> optionalFileName= Optional.empty();
+		Optional<Integer> optionalColumns = Optional.empty();
+		Optional<String> optionalFileName = Optional.empty();
 		
 		for (String arg : args)
 		{
@@ -44,41 +46,31 @@ public class Main
 		}
 		
 		
-		Scanner input = new Scanner(System.in);
-		sorted = optionalSorted.orElseGet(() -> doYouWantToSort(input));
-		columns = optionalColumns.orElseGet(() -> howManyColumns(input));
-		fileName = optionalFileName.orElseGet(() -> getFileName(input));
-	
+		sorted = optionalSorted.orElseGet(Main::doYouWantToSort);
+		columns = optionalColumns.orElseGet(Main::howManyColumns);
+		fileName = optionalFileName.orElseGet(Main::getFileName);
+		
 		//System.out.printf("sort: %s, columns: %s, fileName: %s",sorted,columns,fileName); For Debug Purposes
-		StreamsToColumns.PrintColumns(
-			StreamsToColumns.readStream(!sorted
-				? Objects.requireNonNull(InputRead.readFile(fileName))
-				: Objects.requireNonNull(InputRead.readFile(fileName)).sorted()
-				,columns));
+		StreamsToColumns.PrintColumns(StreamsToColumns.readStream(!sorted ? Objects.requireNonNull(InputRead.readFile(fileName)) : Objects.requireNonNull(InputRead.readFile(fileName)).sorted(), columns));
 	}
 	
 	/**
 	 * Checks if the user wants to sort the text file or not.
-	 * @param input
 	 * @return boolean
 	 */
-	public static boolean doYouWantToSort(Scanner input)
+	private static boolean doYouWantToSort()
 	{
-		String user = "";
-		System.out.printf("Do you wish to sort the list?\n" +
-			"Please type y for yes or n for no\n");
+		String user;
+		System.out.print("Do you wish to sort the list?\n" + "Please type y for yes or n for no\n");
 		user = input.next();
-		if(user.equalsIgnoreCase("y"))
-			return true;
-		return false;
+		return user.equalsIgnoreCase("y");
 	}
 	
 	/**
 	 * Checks how many columns the user wants to have.
-	 * @param input
 	 * @return int
 	 */
-	public static int howManyColumns(Scanner input)
+	private static int howManyColumns()
 	{
 		int user = 0;
 		boolean valid = false;
@@ -90,28 +82,27 @@ public class Main
 			{
 				user = input.nextInt();
 				valid = true;
-			} catch (Exception e)
+			}
+			catch (Exception e)
 			{
 				System.out.println("Need to choose a number.");
 				input.next();
 			}
 			
-		}while(!valid);
+		}
+		while (!valid);
 		
 		return user;
 	}
 	
 	/**
 	 * Asks the user for the filename they wish to read.
-	 * @param input
 	 * @return String
 	 */
-	public static String getFileName(Scanner input)
+	private static String getFileName()
 	{
-		String user = "";
 		System.out.println("Please type the file path, include the extension. If you want it to work that is.");
-		user = input.next();
-		return user;
+		return input.next();
 	}
 	
 	
